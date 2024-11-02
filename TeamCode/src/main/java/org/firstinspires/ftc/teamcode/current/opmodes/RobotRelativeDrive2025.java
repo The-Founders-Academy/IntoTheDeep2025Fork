c216c5d43bf51ad78bb57acf268f51b5b566703a
IanP07 <iannpanait@gmail.com>
2024-11-02 15:02:23 -0400

    Changed Variables in RobotRelativeDrive2025 from local arm variables to Arm2025 subsystem arm variables, so changing them will change both the RobotRelativeDrive and the field relative drive.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/current/opmodes/RobotRelativeDrive2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/current/opmodes/RobotRelativeDrive2025.java
@@ -12,10 +12,16 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.current.subsytems.Arm2025;
+import org.firstinspires.ftc.teamcode.current.subsytems.Mecanum2025;
 
 @TeleOp()
 //@Disabled
 public class RobotRelativeDrive2025 extends LinearOpMode {
+
+
+    public Mecanum2025 m_mecanumDrive;
+    public Arm2025 m_armSubsystem;
 
     public MotorEx m_frontLeft, m_frontRight, m_backLeft, m_backRight;
 
@@ -31,30 +37,12 @@ public class RobotRelativeDrive2025 extends LinearOpMode {
                     * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
                     * 1 / 360.0; // we want ticks per degree, not per rotation
 
-    final double ARM_COLLAPSED_INTO_ROBOT = 0;
-    final double ARM_COLLECT = 239.5 * ARM_TICKS_PER_DEGREE;
-    final double ARM_CLEAR_BARRIER = 220 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SPECIMEN = 151.5 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SAMPLE_IN_LOW = 140 * ARM_TICKS_PER_DEGREE;
-    final double ARM_ATTACH_HANGING_HOOK = 120 * ARM_TICKS_PER_DEGREE;
-    final double ARM_WINCH_ROBOT = 15 * ARM_TICKS_PER_DEGREE;
-
-
-    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
-    final double INTAKE_COLLECT = -1.0;
-    final double INTAKE_OFF = 0.0;
-    final double INTAKE_DEPOSIT = 0.5;
-
-
-    /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
-    final double WRIST_FOLDED_IN = 0.8333;
-    final double WRIST_FOLDED_OUT = 0.5;
 
     /* A number in degrees that the triggers can adjust the arm position by */
     final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
 
     /* Variables that are used to set the arm to a specific position in later functions*/
-    double armPosition = (int) ARM_COLLAPSED_INTO_ROBOT;
+    double armPosition = (int) m_armSubsystem.getARM_COLLAPSED_INTO_ROBOT();
     double armPositionFudgeFactor;
 
 
@@ -114,10 +102,7 @@ public class RobotRelativeDrive2025 extends LinearOpMode {
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
-            // resets field relative drive forward direction to where the robot is currently facing when pressed
-            if (gamepad1.options) {
-                imu.resetYaw();
-            }
+
 
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
@@ -141,6 +126,14 @@ public class RobotRelativeDrive2025 extends LinearOpMode {
             m_frontRight.set(frontRightPower);
             m_backRight.set(backRightPower);
 
+            // DRIVER COMMANDS
+
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+
+            // OPERATOR COMMANDS
 
             /* Here we handle the three buttons that have direct control of the intake speed.
             These control the continuous rotation servo that pulls elements into the robot,
@@ -150,14 +143,18 @@ public class RobotRelativeDrive2025 extends LinearOpMode {
             And if the user presses B it reveres the servo to spit out the element.*/
 
 
-            if (gamepad1.left_bumper) {
-                intake.setPower(INTAKE_COLLECT);
+
+
+
+
+            if (gamepad2.left_bumper) {
+                intake.setPower(m_armSubsystem.getINTAKE_COLLECT());
             }
-            else if (gamepad1.right_bumper) {
-                intake.setPower(INTAKE_DEPOSIT);
+            else if (gamepad2.right_bumper) {
+                intake.setPower(m_armSubsystem.getINTAKE_DEPOSIT());
             }
-            else if (gamepad1.y) {
-                intake.setPower(INTAKE_OFF);
+            else if (gamepad2.y) {
+                intake.setPower(m_armSubsystem.getINTAKE_OFF());
             }
 
 
@@ -178,9 +175,9 @@ public class RobotRelativeDrive2025 extends LinearOpMode {
 
             if(gamepad2.a){
                 /* This is the intaking/collecting arm position */
-                armPosition = ARM_COLLECT;
+                armPosition = m_armSubsystem.getARM_COLLECT();
 //                liftPosition = LIFT_COLLAPSED;
-                wrist.setPosition(WRIST_FOLDED_OUT);
+                wrist.setPosition(m_armSubsystem.getWRIST_FOLDED_OUT());
 //                intake.setPower(robot.INTAKE_COLLECT);
 
             }
@@ -190,41 +187,40 @@ public class RobotRelativeDrive2025 extends LinearOpMode {
                     Note here that we don't set the wrist position or the intake power when we
                     select this "mode", this means that the intake and wrist will continue what
                     they were doing before we clicked left bumper. */
-                armPosition = ARM_CLEAR_BARRIER;
+                armPosition = m_armSubsystem.getARM_CLEAR_BARRIER();
             }
 
             else if (gamepad2.x){
                 /* This is the correct height to score the sample in the LOW BASKET */
-                armPosition = ARM_SCORE_SAMPLE_IN_LOW;
+                armPosition = m_armSubsystem.getARM_SCORE_SAMPLE_IN_LOW();
             }
 
             else if (gamepad2.dpad_down) {
                     /* This turns off the intake, folds in the wrist, and moves the arm
                     back to folded inside the robot. This is also the starting configuration */
-                armPosition = ARM_COLLAPSED_INTO_ROBOT;
+                armPosition = m_armSubsystem.getARM_COLLAPSED_INTO_ROBOT();
                 //liftPosition = LIFT_COLLAPSED;
-//                intake.setPower(robot.INTAKE_OFF);
-                wrist.setPosition(WRIST_FOLDED_IN);
+                wrist.setPosition(m_armSubsystem.getWRIST_FOLDED_IN());
             }
 
             else if (gamepad2.dpad_left){
                 /* This is the correct height to score SPECIMEN on the HIGH CHAMBER */
-                armPosition = ARM_SCORE_SPECIMEN;
-                wrist.setPosition(WRIST_FOLDED_IN);
+                armPosition = m_armSubsystem.getARM_SCORE_SPECIMEN();
+                wrist.setPosition(m_armSubsystem.getWRIST_FOLDED_IN());
             }
 
             else if (gamepad2.dpad_up){
                 /* This sets the arm to vertical to hook onto the LOW RUNG for hanging */
-                  armPosition = ARM_ATTACH_HANGING_HOOK;
+                  armPosition = m_armSubsystem.getARM_ATTACH_HANGING_HOOK();
 //                robot.intake.setPower(robot.INTAKE_OFF);
-                  wrist.setPosition(WRIST_FOLDED_IN);
+                  wrist.setPosition(m_armSubsystem.getWRIST_FOLDED_IN());
             }
 
             else if (gamepad2.dpad_right){
                 /* this moves the arm down to lift the robot up once it has been hooked */
-                  armPosition = ARM_WINCH_ROBOT;
+                  armPosition = m_armSubsystem.getARM_WINCH_ROBOT();
 //                robot.intake.setPower(robot.INTAKE_OFF);
-                  wrist.setPosition(WRIST_FOLDED_IN);
+
             }
 
 //            else if (gamepad2.y) {
