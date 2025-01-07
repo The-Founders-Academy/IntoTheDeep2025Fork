f348e67c958f4888ba4a82753c2357c872575759
evanjducas <evan.j.ducas@gmail.com>
2025-01-07 16:17:54 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/current/testing/LiftMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/current/testing/LiftMotorTest.java
@@ -2,21 +2,19 @@ package org.firstinspires.ftc.teamcode.current.testing;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class LiftMotorTest extends LinearOpMode {
 
     @Config
     public static class TestParams {
-
         public static double LIFT_SCORING_IN_HIGH_BASKET = 0.1;
     }
-    public DcMotor liftMotor = null; //the lift motor
+
+    public DcMotorEx liftMotor = null; // The lift motor
 
     final double LIFT_TICKS_PER_MM = (111132.0 / 289.0) / 120.0;
 
@@ -26,40 +24,38 @@ public class LiftMotorTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        liftMotor = hardwareMap.get(DcMotor.class, "lift");
+        liftMotor = hardwareMap.get(DcMotorEx.class, "lift");
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor.setTargetPosition(0);
         liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
-        /* Run until the driver presses stop */
         while (opModeIsActive()) {
-
-
             if (gamepad1.right_bumper) {
-                 liftPosition = TestParams.LIFT_SCORING_IN_HIGH_BASKET;
-
+                liftPosition = TestParams.LIFT_SCORING_IN_HIGH_BASKET;
             } else if (gamepad1.left_bumper) {
                 liftPosition = LIFT_COLLAPSED;
             }
 
+            liftPosition = Math.max(LIFT_COLLAPSED, Math.min(liftPosition, TestParams.LIFT_SCORING_IN_HIGH_BASKET));
 
+            liftMotor.setTargetPosition((int) liftPosition);
 
-            if (liftPosition > TestParams.LIFT_SCORING_IN_HIGH_BASKET){
-                liftPosition = TestParams.LIFT_SCORING_IN_HIGH_BASKET;
+            liftMotor.setPower(1.0);
+
+            while (liftMotor.isBusy() && opModeIsActive()) {
+                telemetry.addData("Lift Position", liftMotor.getCurrentPosition());
+                telemetry.addData("Target Position", liftMotor.getTargetPosition());
+                telemetry.update();
             }
-            //same as above, we see if the lift is trying to go below 0, and if it is, we set it to 0.
-            if (liftPosition < 0){
-                liftPosition = 0;
-            }
 
-            liftMotor.setTargetPosition((int) (liftPosition));
+            liftMotor.setPower(0.0);
 
-            ((DcMotorEx) liftMotor).setVelocity(2100);
-            liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            telemetry.addData("lift current position", liftMotor.getCurrentPosition());
+            telemetry.addData("Lift Current Position", liftMotor.getCurrentPosition());
+            telemetry.addData("Lift Target Position", liftMotor.getTargetPosition());
             telemetry.update();
         }
     }
