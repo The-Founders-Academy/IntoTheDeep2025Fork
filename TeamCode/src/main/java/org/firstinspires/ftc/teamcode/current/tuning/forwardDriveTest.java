da1b76885d7b7d931a7a69507938f2ea349a322e
ian <iannpanait@gmail.com>
2025-01-18 22:06:48 -0500

    Added tuning files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/current/tuning/forwardDriveTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.current.tuning;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class forwardDriveTest extends LinearOpMode {
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+        // Make sure your motors are named "fL", "fR", "bL", and "bR" in your driverstation config file
+        frontLeft = hardwareMap.get(DcMotor.class, "fL");
+        frontRight = hardwareMap.get(DcMotor.class, "fR");
+        backLeft = hardwareMap.get(DcMotor.class, "bL");
+        backRight = hardwareMap.get(DcMotor.class, "bR");
+
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+
+        // NOTE: This opmode will start moving and NOT stop until you stop the program on the Driverstation
+
+        while(opModeIsActive()) {
+            frontLeft.setPower(1);
+            backLeft.setPower(1);
+            backRight.setPower(1);
+            frontRight.setPower(1);
+
+        }
+    }
+
+
+
+
+}
